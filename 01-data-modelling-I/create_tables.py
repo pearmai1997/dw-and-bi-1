0b1c59687b70785b85d9c6084648e361f1006592
Fooklnwza007 <131597296+Fooklnwza007@users.noreply.github.com>
2024-02-02 03:24:10 +0000

    Add create_tables

--- 01-data-modelling-I/create_tables.py
+++ 01-data-modelling-I/create_tables.py
@@ -6,49 +6,100 @@ import psycopg2
 PostgresCursor = NewType("PostgresCursor", psycopg2.extensions.cursor)
 PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
 
-table_drop_events = "DROP TABLE IF EXISTS events"
-table_drop_actors = "DROP TABLE IF EXISTS actors"
+table_drop_event = "DROP TABLE IF EXISTS event"
+table_drop_actor = "DROP TABLE IF EXISTS actor"
+table_drop_repo = "DROP TABLE IF EXISTS repo"
+table_drop_org = "DROP TABLE IF EXISTS org"
+table_drop_payload = "DROP TABLE IF EXISTS payload"
+table_drop_commit = "DROP TABLE IF EXISTS commit"
 
-table_create_actors = """
-    CREATE TABLE IF NOT EXISTS actors (
-        id int,
-        login text NOT NULL,
-        type text NOT NULL,
-        display_login text,
-        url_actor text,
-        email text NOT NULL,
-        gravatar_id text,
-        avatar_url text,
-        node_id int NOT NULL,
-        PRIMARY KEY(id) 
+table_create_actor = """
+    CREATE TABLE IF NOT EXISTS actor (
+        actor_id int,
+        login varchar(100) NOT NULL,
+        gravatar_id varchar(100),
+        avatar_url varchar(100),
+        PRIMARY KEY(actor_id)
     )
 """
-table_create_events = """
-    CREATE TABLE IF NOT EXISTS events (
-        id text NOT NULL,
-        type text NOT NULL,
-        actor_id int NOT NULL,
-        create_at TIMESTAMP NOT NULL,
-        event_url text,
-        repo_id int NOT NULL,
-        repo_name text NOT NULL,
-        repo_title text,
-        update_at TIMESTAMP NOT NULL,
-        repo_url text,
-        state text,
-        public text NOT NULL, 
-        PRIMARY KEY(id),
-        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
+
+table_create_org = """
+    CREATE TABLE IF NOT EXISTS org (
+        org_id int,
+        login varchar(100) NOT NULL,
+        gravatar_id varchar(100),
+        avatar_url varchar(100),
+        PRIMARY KEY(org_id)
     )
 """
+
+table_create_repo = """
+    CREATE TABLE IF NOT EXISTS repo (
+        repo_id int,
+        name varchar(100) NOT NULL,
+        url varchar(100),
+        PRIMARY KEY(repo_id)
+    )
+"""
+
+table_create_commit = """
+    CREATE TABLE IF NOT EXISTS commit (
+        commit_id varchar(100) PRIMARY KEY,
+        name varchar(100) NOT NULL,
+        "distinct" varchar(100)
+    )
+"""
+table_create_payload = """
+    CREATE TABLE IF NOT EXISTS payload (
+        payload_push_id int,
+        action varchar(100) NOT NULL,
+        node_id varchar(100),
+        type varchar(100),
+        number int,
+        title varchar(100),
+        commit_id varchar(100),
+        PRIMARY KEY(payload_push_id),
+        CONSTRAINT fk_commit FOREIGN KEY(commit_id) REFERENCES commit(commit_id)
+    )
+"""
+
+table_create_event = """
+    CREATE TABLE IF NOT EXISTS event (
+        event_id varchar(100) NOT NULL,
+        event_type varchar(100) NOT NULL,
+        event_url varchar(100) NOT NULL,
+        public boolean NOT NULL,
+        create_at timestamp NOT NULL,
+        actor_id int,
+        repo_id int,
+        payload_push_id int,
+        org_id int,
+        PRIMARY KEY(event_id),
+        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actor(actor_id),
+        CONSTRAINT fk_repo FOREIGN KEY(repo_id) REFERENCES repo(repo_id),
+        CONSTRAINT fk_payload FOREIGN KEY(payload_push_id) REFERENCES payload(payload_push_id),
+        CONSTRAINT fk_org FOREIGN KEY(org_id) REFERENCES org(org_id)
+    )
+"""
+
+
 
 create_table_queries = [
-    table_create_actors,
-    table_create_events,
+    table_create_actor,
+    table_create_org,
+    table_create_repo,
+    table_create_commit,
+    table_create_payload,
+    table_create_event
 ]
+
 drop_table_queries = [
-    table_drop_events,
-    table_drop_actors,
+    table_drop_event,
+    table_drop_actor,
+    table_drop_org,
+    table_drop_repo,
+    table_drop_payload,
+    table_drop_commit
 ]
 
 
